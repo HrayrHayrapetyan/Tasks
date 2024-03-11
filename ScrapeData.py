6e39bad12735c6c7be0e75f58008225a9c6b633d
Picsart Academy <picsartacademy@MacBook-Pro-von-Code.local>
2024-03-11 19:52:57 +0400

    scrape fix

--- ScrapeData.py
+++ ScrapeData.py
@@ -1,19 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 
-   
-def scrape_data(https:str):
-	try:
-		r=requests.get(https)
-		soup=BeautifulSoup(r.text,features='html.parser')
-		#print(soup)
-		with open('scraped.txt','w') as file:
-				for item in soup.find_all(class_=['title','description card-text','ratings','float -end review count']):
-					file.write(str(item))
-	except (FileNotFoundError,AttributeError,NameError,SyntaxError) as exp:
-		print(exp)
+                                                                                                                       		
+def scrape_data(url:str):
+        try:
+            r=requests.get(url)
+            soup=BeautifulSoup(r.text,features='lxml')
+            items=soup.find_all(class_='col-md-4 col-xl-4 col-lg-4')
+            with open('scraped.txt','w') as file:
+                    file.write(str(items))
+        except Exception as exp:
+            print(exp)
 
 scrape_data('https://webscraper.io/test-sites/e-commerce/allinone')
- 
-                                                                                                                      		
-                                                                                                                      		
+
+            
+            
+
+
+         
+                    
+            
+
+        
+        
